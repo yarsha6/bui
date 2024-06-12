78f6fb1a1faaaa7c29022dc92ae4e1a2beb20306
yarsha6 <yarshad@cims.carleton.ca>
2024-06-12 12:02:19 -0400

    feat: first commit of ya bim ui tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,183 +1,129 @@
-import * as BUI from "@thatopen/ui";
-import * as THREE from "three";
-import * as OBC from "@thatopen/components";
+import * as BUI from "@thatopen/ui"
 
-const plansList = document.getElementById("plans-list") as BUI.Table;
-const elevationsList = document.getElementById("elevations-list") as BUI.Table;
-const sectionsList = document.getElementById("sections-list") as BUI.Table;
-// const properties = document.getElementById("properties") as BUI.Table;
+BUI.Manager.registerComponents()
 
-const container = document.getElementById("viewport") as BUI.Viewport;
-const components = new OBC.Components();
+const viewsList = document.getElementById("views-list") as BUI.Table
 
-const worlds = components.get(OBC.Worlds);
-
-const world = worlds.create<
-  OBC.SimpleScene,
-  OBC.SimpleCamera,
-  OBC.SimpleRenderer
->();
-
-world.scene = new OBC.SimpleScene(components);
-world.renderer = new OBC.SimpleRenderer(components, container);
-world.camera = new OBC.SimpleCamera(components);
-
-components.init();
-
-world.scene.three.background = null;
-
-const grids = components.get(OBC.Grids);
-const grid = grids.create(world);
-console.log(grid);
-
-const material = new THREE.MeshLambertMaterial({ color: "#6528D7" });
-const geometry = new THREE.BoxGeometry();
-geometry.translate(0, 0.5, 0);
-const cube = new THREE.Mesh(geometry, material);
-world.scene.three.add(cube);
-
-world.scene.setup();
-
-world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0);
-
-BUI.Manager.init();
-
-const controls = document.getElementById("controls") as HTMLElement;
-
-const panel = BUI.Component.create<BUI.PanelSection>(() => {
-  return BUI.html`
-      <bim-panel label="Worlds Tutorial" class="options-menu">
-        <bim-panel-section collapsed label="Controls">
-        
-          <bim-color-input 
-            label="Background Color" color="#202932" 
-            @input="${({ target }: { target: BUI.ColorInput }) => {
-              world.scene.three.background = new THREE.Color(target.color);
-            }}">
-          </bim-color-input>
-          
-          <bim-number-input 
-            slider step="0.1" label="Directional lights intensity" value="1.5" min="0.1" max="10"
-            @change="${({ target }: { target: BUI.NumberInput }) => {
-              for (const child of world.scene.three.children) {
-                if (child instanceof THREE.DirectionalLight) {
-                  child.intensity = target.value;
-                }
-              }
-            }}">
-          </bim-number-input>
-          
-          <bim-number-input 
-            slider step="0.1" label="Ambient light intensity" value="1" min="0.1" max="5"
-            @change="${({ target }: { target: BUI.NumberInput }) => {
-              for (const child of world.scene.three.children) {
-                if (child instanceof THREE.AmbientLight) {
-                  child.intensity = target.value;
-                }
-              }
-            }}">
-          </bim-number-input>
-          
-        </bim-panel-section>
-      </bim-panel>
-      `;
-});
-
-controls.appendChild(panel);
-
-plansList.data = [
+viewsList.data = [
   {
     data: {
-      Name: "Floor Plans",
+      Name: "Structural Plans"
     },
     children: [
       {
         data: {
-          Name: "Level 01",
-        },
+          Name: "Level 01"
+        }
       },
       {
         data: {
-          Name: "Level 02",
-        },
+          Name: "Level 02"
+        }
       },
       {
         data: {
-          Name: "Level 03",
-        },
+          Name: "Level 03"
+        }
       },
-    ],
+    ]
   },
   {
     data: {
-      Name: "Reflected Ceiling Plans",
+      Name: "Floor Plans"
     },
     children: [
       {
         data: {
-          Name: "Level 01",
-        },
-      },
-      {
-        data: {
-          Name: "Level 02",
-        },
-      },
-      {
-        data: {
-          Name: "Level 03",
-        },
-      },
-    ],
+          Name: "Level 01"
+        }
+      }
+    ]
   },
-];
-
-elevationsList.data = [
   {
     data: {
-      Name: "Elevations",
+      Name: "Ceiling Plans"
     },
     children: [
       {
         data: {
-          Name: "North",
-        },
-      },
-      {
-        data: {
-          Name: "South",
-        },
-      },
-      {
-        data: {
-          Name: "West",
-        },
-      },
-      {
-        data: {
-          Name: "East",
-        },
-      },
-    ],
+          Name: "Level 01"
+        }
+      }
+    ]
   },
-];
-
-sectionsList.data = [
   {
     data: {
-      Name: "Sections",
+      Name: "3D Views"
     },
     children: [
       {
         data: {
-          Name: "Section AA",
-        },
+          Name: "Presentation View"
+        }
+      }
+    ]
+  },
+  {
+    data: {
+      Name: "Elevations"
+    },
+    children: [
+      {
+        data: {
+          Name: "North"
+        }
       },
       {
         data: {
-          Name: "Section BB",
-        },
+          Name: "South"
+        }
       },
-    ],
+      {
+        data: {
+          Name: "East"
+        }
+      },
+      {
+        data: {
+          Name: "West"
+        }
+      }
+    ]
   },
-];
+  {
+    data: {
+      Name: "Sections"
+    },
+    children: [
+      {
+        data: {
+          Name: "Section 01"
+        }
+      }
+    ]
+  },
+  {
+    data: {
+      Name: "Detail Views"
+    },
+    children: [
+      {
+        data: {
+          Name: "Flashing"
+        }
+      }
+    ]
+  },
+  {
+    data: {
+      Name: "Renderings"
+    },
+    children: [
+      {
+        data: {
+          Name: "Exterior Main Entrance"
+        }
+      }
+    ]
+  }
+]
